0d8094be8ac40be4d9cb3457d4a328d3214b1e96
fargila <atilahunesco@gmail.com>
2024-08-19 16:03:07 -0300

    Sprint1-semana2

---
+++ Sprint-01-semana-02/Web/Projeto 01&02&03&04&05&06/main.js
@@ -0,0 +1,34 @@
+//Projeto-01&02
+let _fullName = "<<Átila França do Nascimento>>"
+let _russianIntroduction = "Ya zavut "
+let _legalAge = 22
+
+
+console.log(_russianIntroduction + _fullName)
+console.log("i u menya " + _legalAge + " let")
+
+//--------------------------------------------------------
+//Projeto-03
+// let _firstName = prompt("Digite o seu primeiro nome: ")
+// let _secondName = prompt("Digite o seu sobrenome nome: ")
+// let _age = parseInt(prompt("Digite a sua idade: "))
+// const _importantNumber = 4
+
+// console.log(_firstName)
+// console.log(_secondName)
+// console.log((_age+2)+_importantNumber)
+
+//--------------------------------------------------------
+//Projeto-05
+let _fName2 = prompt("Digite o seu primeiro nome: ")
+let _grettings = "Boa vida,"
+document.getElementById('gretting').innerHTML = _grettings +" "+ _fName2
+
+//--------------------------------------------------------
+//Projeto-06
+let _friends = ['Maicon Küster', 'Neymar Jr', 'Flávio Kotaka', 'Jão']
+console.log(_friends)
+
+_friends[1] = 'Pewdiepie'
+_friends[5] = 'Julius do Todo Mundo Odeia o Chris'
+console.log(_friends)
