06b8da873ab5bcaa3015c49f4dd1d7a23633dfef
fargila <atilahunesco@gmail.com>
2024-09-02 12:13:32 -0300

    Correçãeção dos nomes das pastas SPRINT->SEMANA

---
+++ Semana-05/Projeto Job Finder/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const db = require('./db/connection')
+
+const PORT = 3000
+
+app.listen(PORT, function(){
+    console.log(`O Express está rodando na porta ${PORT}`)
+})
+
+//db conexições
+db.authenticate().then(()=>{
+    console.log("Conectou ao banco de dados")
+}).catch(err => {
+    console.log("Ocorreu um erro ao conectar", err)
+})
+
+//rotas
+app.get('/', (req, res) => {
+    res.send("Está funcionado...")
+})
